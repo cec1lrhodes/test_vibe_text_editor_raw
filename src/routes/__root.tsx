3544268194df391cce24c7caedd7d7ed74f8cc1d
cec1lrhodes <kubalewand21@gmail.com>
2026-04-16 15:25:27 +0300

    fix(devtools): hide TanStackRouterDevtools in production

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,13 @@
 import * as React from "react";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+const TanStackRouterDevtools = import.meta.env.PROD
+  ? () => null
+  : React.lazy(() =>
+      import("@tanstack/router-devtools").then((res) => ({
+        default: res.TanStackRouterDevtools,
+      })),
+    );
 
 export const Route = createRootRoute({
   component: RootComponent,
